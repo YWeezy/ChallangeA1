3329ffeb4515e7c2cceb6e74fc8409ed4af1a769
Wesley <wesleyandrade0305@gmail.com>
2023-10-02 23:44:58 +0200

    push rest story

--- riddle.py
+++ riddle.py
@@ -3,9 +3,9 @@ import time
 riddles = ["What gets bigger the more you take away?", "What gets wet while drying?", "David’s parents have three sons: Snap, Crackle, and what’s the name of the third son?", "I follow you all the time and copy your every move, but you can’t touch me or catch me. What am I?"]
 answers = ["hole", "towel", "david", "shadow"]
 
-print
 def ridd():
     system('clear')
+    done = True
     print ("Welcome to riddles")
     print ("Answer the riddles in low caps and one word !!")
     for ind, x in enumerate(riddles):
@@ -19,7 +19,13 @@ def ridd():
         else:
             print("incorrect")
             time.sleep(2)
-        system('clear')
+            done = False
+            break
+        
+    if done == True:
+        return True
+    else:
+        return False
+        
 
-         
-ridd()
+      
